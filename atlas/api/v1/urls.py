6d959985c03fa82c9cce2042bbdff1b3654de5e0
Wisnu P. Ramadhan <wisnuprama014@gmail.com>
2019-03-04 16:19:14 +0700

    fix url schema

--- atlas/api/v1/urls.py
+++ atlas/api/v1/urls.py
@@ -13,13 +13,13 @@ general = [
 ]
 
 auth = [
-    path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('tokens', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('tokens/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 account = [
     path('register', UserCreateView.as_view(), name='account_register'),
-    path('user/<username>', UserDetailView.as_view(), name='account_user_detail'),
+    path('users/<username>', UserDetailView.as_view(), name='account_user_detail'),
 ]
 
 

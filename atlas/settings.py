5298f41ff52877fc32ed4888532262471c295d11
Wisnu Pramadhitya Ramadhan <wisnu.pramadhitya@ui.ac.id>
2019-03-08 23:47:23 +0700

    Resolve "[RFC] Use AWS S3 as default storage"

--- atlas/settings.py
+++ atlas/settings.py
@@ -50,7 +50,6 @@ MODULES = [
     'django_extensions',
     'rest_framework',
     'storages',
-    'whitenoise.runserver_nostatic',
 ]
 
 INSTALLED_APPS = (
@@ -66,17 +65,30 @@ INSTALLED_APPS = (
     + APPS
 )
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+if PRODUCTION and not TESTING:
+    MIDDLEWARE = [
+        'django.middleware.security.SecurityMiddleware',
+        'corsheaders.middleware.CorsMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ]
+else:
+    # we use whitenoise for local or dev environment for speed
+    MIDDLEWARE = [
+        'django.middleware.security.SecurityMiddleware',
+        'whitenoise.middleware.WhiteNoiseMiddleware',
+        'corsheaders.middleware.CorsMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ]
 
 ROOT_URLCONF = 'atlas.urls'
 
@@ -230,9 +242,22 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-DEFAULT_PROFILE_PIC = f'{MEDIA_URL}img/default-profile-pic.jpeg'
+DEFAULT_PROFILE_PIC = 'img/default-profile-pic.jpeg'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Storage settings
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+if PRODUCTION and not TESTING:
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_STORAGE_BUCKET_NAME = 'b3-mnemosyne'
+else:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    AWS_STORAGE_BUCKET_NAME = 'b3-mnemosyne-dev'
 
-# @todo: amazon s3 for storage
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_S3_FILE_OVERWRITE=False
+AWS_QUERYSTRING_AUTH = False
+AWS_DEFAULT_ACL='public-read'
+
+
 # @todo: raven for debug production

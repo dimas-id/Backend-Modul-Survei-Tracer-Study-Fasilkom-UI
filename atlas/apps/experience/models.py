9bee8baaf92bc2023573cc6aa8baaf1546b56a82
Wisnu P. Ramadhan <wisnuprama014@gmail.com>
2019-04-05 19:52:27 +0700

    remove unused field and add company_metadata -> useful

--- atlas/apps/experience/models.py
+++ atlas/apps/experience/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils.translation import ugettext as _
+from django.contrib.postgres.fields import JSONField
+from django.utils.translation import ugettext as _
 
 from atlas.libs.db.models import AbstractDateCreatedRecordable
 
@@ -16,12 +18,18 @@ class Position(AbstractDateCreatedRecordable):
 
     title = models.CharField(_('Title'), max_length=64)
     company_name = models.CharField(_('Company'), max_length=64)
-    description = models.TextField(_('Description'))
-    location_name = models.CharField(_('Location'), max_length=64)
+    location_name = models.CharField(_('Location'), max_length=64, null=True)
     industry_name = models.CharField(_('Industry'), max_length=64)
 
     date_started = models.DateField(_('Date Started'))
     date_ended = models.DateField(_('Date Ended'), null=True, blank=True)
+
+    company_metadata = JSONField(null=True, blank=True, help_text=_(
+        'Here is company metadata comes from linkedin'))
+
+    @property
+    def is_current(self):
+        return self.date_ended is None
 
 
 class Education(AbstractDateCreatedRecordable):

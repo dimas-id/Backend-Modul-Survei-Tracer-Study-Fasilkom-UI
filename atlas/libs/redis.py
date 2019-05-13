7f6556901e5d1ffac05c41fc7a5571490820c60e
Wisnu P. Ramadhan <wisnuprama014@gmail.com>
2019-05-13 20:18:39 +0700

    turn off redis when testing

--- atlas/libs/redis.py
+++ atlas/libs/redis.py
@@ -1,7 +1,10 @@
+from django.conf import settings
 import django_rq
 
 __default__ = django_rq.get_queue('default')
 
 
 def enqueue(func, *args, **kwargs):
+    if settings.TESTING:
+        return
     __default__.enqueue(func, *args, **kwargs)

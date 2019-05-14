27870847b5d3219ccdfa643b0f25a895cc57e0b6
Wisnu Pramadhitya <wisnuprama014@gmail.com>
2019-05-14 17:16:00 +0000

    modified start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env sh
 
+git pull origin master
+
 source ./.env
 
-python manage.py collectstatic --no-input
-python manage.py migrate
-python manage.py compilemessages
+pipenv install --deploy
 
-gunicorn atlas.wsgi:application --bind 0.0.0.0:8000 --error-logfile error.log 
+# python manage.py collectstatic --no-input
+pipenv run python manage.py migrate
+pipenv run python manage.py compilemessages
+
+nohup pipenv run gunicorn atlas.wsgi:application \
+	--bind 0.0.0.0:8000 \
+	--access-logfile - \
+	--error-logfile error.log \
+	--workers=3 \
+	> /dev/null & echo $(($!+1)) > atlas.pid
+
+nohup pipenv run python manage.py rqworker > /dev/null & echo $(($!+1)) > atlas_rq.pid

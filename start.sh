fe7abac76ab169b923fda9f92157811594485496
Wisnu Pramadhitya <wisnu.pramadhitya@ui.ac.id>
2019-05-14 11:09:51 +0700

    update start sh script

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
-# tidak mau jalan pake ini
 
-python manage.py collectstatic --noinput
+source ./.env
+
+python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py compilemessages
 
-gunicorn atlas.wsgi:application --bind 0.0.0.0:8000 --error-logfile error.log --workers 3
+gunicorn atlas.wsgi:application --bind 0.0.0.0:8000 --error-logfile error.log 

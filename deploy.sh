5d8067ba216cec224950b9080f289583eb687f4d
Wisnu P. Ramadhan <wisnuprama014@gmail.com>
2019-05-15 10:44:35 +0700

    update deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,3 @@
 # !/bin/sh
-echo 'SIGHUP Atlas process'
-ssh wisnuprama@165.22.244.180 'kill -HUP $(<"/home/wisnuprama/iluni12/b3-atlas/atlas.pid")'
-
-echo 'SIGHUP Atlas RQWorker process'
-ssh wisnuprama@165.22.244.180 'kill -HUP $(<"/home/wisnuprama/iluni12/b3-atlas/atlas_rq.pid")'
-
 echo 'RUN Deployment'
 ssh wisnuprama@165.22.244.180 'cd /home/wisnuprama/iluni12/b3-atlas/ && bash start.sh'
--- start.sh
+++ start.sh
@@ -1,26 +1,40 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
-git pull origin master
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
 
-echo 'LOAD ENV VARIABLES'
-source ./.env
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    echo 'LOAD ENV VARIABLES'
+		source ./.env
 
-pipenv install --deploy
+    echo 'INSTALL DEPENDENCIES'
+		pipenv install --deploy
 
-echo 'RUN MIGRATION'
-# python manage.py collectstatic --no-input
-pipenv run python manage.py migrate
-pipenv run python manage.py compilemessages
+		echo 'RUN MIGRATION'
+		# python manage.py collectstatic --no-input
+		pipenv run python manage.py migrate
+		pipenv run python manage.py compilemessages
 
-echo 'RUN GUNICORN BIND ON 8000'
-nohup pipenv run gunicorn atlas.wsgi:application \
-	--bind 0.0.0.0:8000 \
-	--access-logfile - \
-	--error-logfile error.log \
-	--workers=3 \
-	> /dev/null & echo $(($!+1)) > atlas.pid
+		echo 'SIGHUP Atlas process'
+		kill -HUP $(<"/home/wisnuprama/iluni12/b3-atlas/atlas.pid")
 
-echo 'RUN RQWORKER'
-nohup pipenv run python manage.py rqworker > /dev/null & echo $(($!+1)) > atlas_rq.pid
+		echo 'SIGHUP Atlas RQWorker process'
+		kill -HUP $(<"/home/wisnuprama/iluni12/b3-atlas/atlas_rq.pid")
 
-echo 'ATLAS DEPLOYMENT SUCCESS'
+		echo 'RUN GUNICORN BIND ON 8000'
+		nohup pipenv run gunicorn atlas.wsgi:application \
+			--bind 0.0.0.0:8000 \
+			--access-logfile - \
+			--error-logfile error.log \
+			--workers=3 \
+			> /dev/null & echo $(($!+1)) > atlas.pid
+
+		echo 'RUN RQWORKER'
+		nohup pipenv run python manage.py rqworker > /dev/null & echo $(($!+1)) > atlas_rq.pid
+
+		echo 'ATLAS DEPLOYMENT SUCCESS'
+fi

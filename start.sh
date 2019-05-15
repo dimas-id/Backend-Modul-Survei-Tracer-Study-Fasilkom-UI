85a9fb038ecd9c9d7944a3eb83b06ea9fa4a0cd0
Wisnu P. Ramadhan <wisnuprama014@gmail.com>
2019-05-15 10:35:38 +0700

    add deployment script

--- start.sh
+++ start.sh
@@ -2,14 +2,17 @@
 
 git pull origin master
 
+echo 'LOAD ENV VARIABLES'
 source ./.env
 
 pipenv install --deploy
 
+echo 'RUN MIGRATION'
 # python manage.py collectstatic --no-input
 pipenv run python manage.py migrate
 pipenv run python manage.py compilemessages
 
+echo 'RUN GUNICORN BIND ON 8000'
 nohup pipenv run gunicorn atlas.wsgi:application \
 	--bind 0.0.0.0:8000 \
 	--access-logfile - \
@@ -17,4 +20,7 @@ nohup pipenv run gunicorn atlas.wsgi:application \
 	--workers=3 \
 	> /dev/null & echo $(($!+1)) > atlas.pid
 
+echo 'RUN RQWORKER'
 nohup pipenv run python manage.py rqworker > /dev/null & echo $(($!+1)) > atlas_rq.pid
+
+echo 'ATLAS DEPLOYMENT SUCCESS'

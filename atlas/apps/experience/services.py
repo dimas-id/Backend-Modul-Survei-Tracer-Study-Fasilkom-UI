8b0e7dfbae77e50f20fd0dace279615fa6c6aca9
vianhazman <vianhazman@gmail.com>
2020-01-23 13:48:41 +0700

    [ELASTIIC]

--- atlas/apps/experience/services.py
+++ atlas/apps/experience/services.py
@@ -5,13 +5,13 @@ from django.db import transaction
 from atlas.apps.account.constants import C_UI_PROGRAMS_FIELD, C_UI_STATUS, C_UI_NAME_FIELD
 from atlas.apps.experience.utils import check_verified_status
 from atlas.clients.csui import StudentManager
+from atlas.clients.elastic import ElasticManager
 from .models import Position, Education
 from atlas.apps.experience.utils import (
     validate_alumni_data, extract_alumni_data, get_most_matching_mahasiswa)
 
 
 class ExperienceService:
-    student_manager = StudentManager()
 
     def extract_and_create_positions_from_linkedin(self, user, positions):
         list_pos = []
@@ -52,25 +52,35 @@ class ExperienceService:
           then we need to iterate all O(N) and find the right user.
         * because latency
         """
+
+        is_siak = (education.csui_program[:2] == "S2" and education.csui_class_year > 2004) or \
+                  (education.csui_program[:2] == "S1" and education.csui_class_year > 2001)
+
+        student_manager = StudentManager() if is_siak else ElasticManager()
+
         user_npm = getattr(education, 'ui_sso_npm', None)
         mahasiswa_data = None
-        if user_npm is not None:
-            mahasiswa_data, success, _ = self.student_manager.get_student_by_npm(
-                user_npm)
+        if user_npm is not None and user_npm.strip() != '':
+            print(user_npm)
+            mahasiswa_data, success = student_manager.get_student_by_npm(user_npm)
         else:
             # how if the api is using paginator? LOL
-            user_fullname = [getattr(education.user, 'first_name'),getattr(education.user, 'last_name', None)]
+            user_fullname = [getattr(education.user, 'first_name'), getattr(education.user, 'last_name', None)]
             user_fullname_concat = getattr(education.user, 'name')
             mahasiswa_list = []
-            for name in user_fullname:
-                if name:
-                    mahasiswa_list_temp, success, _ = self.student_manager.get_students_by_name(
-                        name)
-                    mahasiswa_list.extend(mahasiswa_list_temp)
+            if is_siak:
+                for name in user_fullname:
+                    if name and name.strip():
+                        mahasiswa_list_temp, success, _ = student_manager.get_students_by_name(
+                            name)
+                        mahasiswa_list.extend(mahasiswa_list_temp)
+            else:
+                mahasiswa_list, success = student_manager.get_students_by_name(user_fullname_concat)
             # we must find the mhs using name
             if success:
                 mahasiswa_data, _ = get_most_matching_mahasiswa(
-                    mahasiswa_list, user_fullname_concat, lambda mhs_data: mhs_data.get(C_UI_NAME_FIELD), education.csui_program[:2])
+                    mahasiswa_list, user_fullname_concat, lambda mhs_data: mhs_data.get(C_UI_NAME_FIELD), \
+                    education.csui_program[:2])
         is_valid = False
         if mahasiswa_data is not None and success:
             # not none then we validatez
@@ -92,14 +102,10 @@ class ExperienceService:
                     C_UI_STATUS) if len(mahasiswa_data.get(C_UI_PROGRAMS_FIELD)) > 0 else 'Kosong'
                 setattr(education, 'csui_graduation_status',
                         latest_status)
-
-                education.user.profile.save(update_fields=('birthdate',))
-                education.save(update_fields=('ui_sso_npm','csui_program','csui_class_year','csui_graduation_status'))
+                if Education.objects.filter(id=education.id).exists():
+                    education.user.profile.save(update_fields=('birthdate',))
+                    education.save(update_fields=('ui_sso_npm','csui_program','csui_class_year','csui_graduation_status'))
         #Change user verification status
         check_verified_status(education.user)
 
         return is_valid, mahasiswa_data
-
-    # def verify_user_profile(sender, instance: UserProfile, created, **kwargs):
-    #     if not created and not instance.user.is_verified:
-    #         redis.enqueue(user_service.verify_user_registration, user=instance.user)
